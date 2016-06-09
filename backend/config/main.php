150bcd41b9082d33ce4a05aab1dc052045f432a3
caiqinglin <caiqinglinbeihai@gmail.com>
2016-06-09 10:59:02 +0800

    init

--- backend/config/main.php
+++ backend/config/main.php
@@ -7,7 +7,7 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-api',
+    'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
